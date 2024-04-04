f6fa748d283972c165502dd430eb0c77cbfe1230
jean <lucmartinsjean@gmail.com>
2024-04-04 19:24:23 -0300

    projeto login

---
+++ framework/projetos/login.js
@@ -0,0 +1,15 @@
+var login = prompt("digite seu usuario ");
+var senha = prompt ("digite sua senha ");
+
+if(login)
+{
+    console.log(+login);
+}
+if(senha)
+{
+    console.log(+senha);
+}
+if(login++senha==usuario)
+{
+    console.log("Seja Bem Vindo" + usuario)
+}
